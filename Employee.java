088880c6114b118abc83793484c86c105f43c19b
guru <guravaiah.malli@gmail.com>
2021-05-05 12:50:46 +0530

    modified Employee.java

--- Employee.java
+++ Employee.java
@@ -1,9 +1,9 @@
 class Emp{
 public static void main(String args[]){
-int Emp_no;
-string name;
-char sex;
-float salary;
+int Emp_no=100;
+string name=GURU;
+char sex=M;
+float salary=1LAKH;
 System.out.println("Emp no:"+Emp_no);
 System.out.println("EmpName:"+name);
 System.out.println("Gender:"+sex);
