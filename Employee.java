ed1e9c49ba2881aeede3e2d020fd9338ad06ec82
guru <guravaiah.malli@gmail.com>
2021-05-05 12:42:08 +0530

    Inital Commit

---
+++ Employee.java
@@ -0,0 +1,11 @@
+class Emp{
+public static void main(String args[]){
+int Emp_no;
+string name;
+char sex;
+float salary;
+System.out.println("Emp no:"+Emp_no);
+System.out.println("EmpName:"+name);
+System.out.println("Gender:"+sex);
+}
+}
